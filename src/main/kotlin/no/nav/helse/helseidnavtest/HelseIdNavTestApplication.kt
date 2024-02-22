69f8b90bdd5a606ef7e9509feb6e28b8858ca454
Jan-Olav Eide <jan.olav.eide@gmail.com>
2024-02-22 09:59:37 +0100

    map granted authority

--- src/main/kotlin/no/nav/helse/helseidnavtest/HelseIdNavTestApplication.kt
+++ src/main/kotlin/no/nav/helse/helseidnavtest/HelseIdNavTestApplication.kt
@@ -1,17 +1,20 @@
 package no.nav.helse.helseidnavtest
 
 import com.nimbusds.jose.jwk.JWK
-import no.nav.boot.conditionals.Cluster.Companion.profiler
+import kotlin.math.log
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
 import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient
 import org.springframework.security.oauth2.client.endpoint.NimbusJwtClientAuthenticationParametersConverter
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter
@@ -21,13 +24,17 @@ import org.springframework.security.oauth2.client.web.DefaultOAuth2Authorization
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers.withPkce
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import no.nav.boot.conditionals.Cluster.Companion.profiler
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 class HelseIdNavTestApplication
 
 fun main(args: Array<String>) {
@@ -37,13 +44,39 @@ fun main(args: Array<String>) {
 }
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 @EnableWebSecurity(debug = true)
 class SecurityConfig(@Value("\${helse-id.jwk}") private val assertion: String) {
     private val jwk = JWK.parse(assertion)
 
+    private val log = LoggerFactory.getLogger(SecurityConfig::class.java)
+    @Bean
+     fun userAuthoritiesMapper() : GrantedAuthoritiesMapper {
+        return GrantedAuthoritiesMapper { authorities : Collection<GrantedAuthority> ->
+            val mappedAuthorities  = mutableSetOf<GrantedAuthority>()
+            authorities.forEach { authority ->
+                log.warn("Authority {}", authority.authority)
+                mappedAuthorities.add(authority)
+                if (OidcUserAuthority::class.java.isInstance(authority)) {
+                    val oidcUserAuthority = authority as OidcUserAuthority
+                    val idToken = oidcUserAuthority.idToken
+                    if (authority.authority == "OIDC_USER") {
+                        val level = idToken.getClaim<String>("helseid://claims/identity/security_level")
+                        val hpr = idToken.getClaim<String>("helseid://claims/hpr/hpr_number")
+                        if ("4" == level && hpr != null) {
+                            mappedAuthorities.add(GrantedAuthority { "LEGE_4" })
+                        }
+                    }
+                }
+            }
+            mappedAuthorities
+        }
+    }
     @Bean
     fun oidcLogoutSuccessHandler(repo: ClientRegistrationRepository) =
-        OidcClientInitiatedLogoutSuccessHandler(repo).apply { setPostLogoutRedirectUri("http://www.vg.no") }
+        OidcClientInitiatedLogoutSuccessHandler(repo).apply {
+            setPostLogoutRedirectUri("{baseUrl}/oauth2/authorization/helseid" )
+        }
 
     @Bean
     fun requestEntityConverter() = OAuth2AuthorizationCodeGrantRequestEntityConverter().apply {
@@ -80,13 +113,19 @@ class SecurityConfig(@Value("\${helse-id.jwk}") private val assertion: String) {
                     authorizationRequestResolver = resolver
                 }
             }
+            oauth2ResourceServer {
+                jwt {
+                }
+            }
             oauth2Client {}
             logout {
                 logoutSuccessHandler = successHandler
             }
             authorizeRequests {
+                authorize("/hello1", authenticated)
                 authorize("/public/**", permitAll)
                 authorize("/actuator/**", permitAll)
+                authorize("/hello", hasAuthority("LEGE_4"))
                 authorize(anyRequest, authenticated)
             }
         }
@@ -97,6 +136,7 @@ class SecurityConfig(@Value("\${helse-id.jwk}") private val assertion: String) {
 }
 
 @RestController
+@EnableMethodSecurity(prePostEnabled = true)
 class HelseController {
 
     private val log = LoggerFactory.getLogger(HelseController::class.java)
@@ -104,24 +144,50 @@ class HelseController {
     @GetMapping("/")
     fun root() = "root"
 
-    @GetMapping("/public/utlogget")
-    fun public() = "Du er nå logget ut. <a href='/hello'>Logg inn igjen</a>"
+    @GetMapping("/error")
+    fun error() = "Uh-oh"
 
-    @GetMapping("/hello")
-    fun hello(authentication: Authentication): String {
+    @GetMapping("/hello1")
+    fun hello1(authentication: Authentication): String {
         val oidcUser = authentication.principal as OidcUser
         val attributes = oidcUser.attributes
         val idToken = oidcUser.idToken
-
+        log.info(oidcUser.userInfo.claims.toString())
         log.warn("Logget inn bruker (pseudo) personnummer: {}", attributes["helseid://claims/identity/pid_pseudonym"])
         log.warn("{}", idToken.tokenValue)
+        idToken.claims.forEach { log.warn("{}", it) }
+        val auths = oidcUser.authorities.joinToString { it.authority }
 
         return """
             <p>Hello from <b>${attributes["name"]}</b></p>
             <p>HPR-nummer: <b>${attributes["helseid://claims/hpr/hpr_number"]}</b></p>
             <p>Nivå: <b>${attributes["helseid://claims/identity/assurance_level"]}</b> - <b>${attributes["helseid://claims/identity/security_level"]}</b></p>
             <p>Verifisert med: <b>${attributes["idp"]}</b></p>
-                
+            <p>Authorities <b>$auths}</b></p>
+
+            <a href="/logout"><button>Logg ut</button></a>
+        """.trimIndent()
+    }
+    @GetMapping("/public/utlogget")
+    fun public() = "Du er nå logget ut. <a href='/hello'>Logg inn igjen</a>"
+
+    @GetMapping("/hello")
+    fun hello(authentication: Authentication): String {
+        val oidcUser = authentication.principal as OidcUser
+        val attributes = oidcUser.claims
+        val idToken = oidcUser.idToken
+        val auths = oidcUser.authorities.joinToString { it.authority }
+        println("AAA")
+        log.warn("Logget inn bruker (pseudo) personnummer: {}", attributes["helseid://claims/identity/pid_pseudonym"])
+        log.warn("{}", idToken.tokenValue)
+        idToken.claims.forEach { log.warn("{}", it) }
+
+        return """
+            <p>Hello from <b>${attributes["name"]}</b></p>
+            <p>HPR-nummer: <b>${attributes["helseid://claims/hpr/hpr_number"]}</b></p>
+            <p>Nivå: <b>${attributes["helseid://claims/identity/assurance_level"]}</b> - <b>${attributes["helseid://claims/identity/security_level"]}</b></p>
+            <p>Verifisert med: <b>${attributes["idp"]}</b></p>
+            <p>Authorities <b>$auths}</b></p>
             <a href="/logout"><button>Logg ut</button></a>
         """.trimIndent()
     }
