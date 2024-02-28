41b8630bb4e64d4565a405e0c8afc5db4c23317e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2024-02-28 14:40:15 +0100

    cleanup

--- src/main/kotlin/no/nav/helse/helseidnavtest/helseopplysninger/ClaimsExtractor.kt
+++ src/main/kotlin/no/nav/helse/helseidnavtest/helseopplysninger/ClaimsExtractor.kt
@@ -46,12 +46,12 @@ class ClaimsExtractor(private val claims : Map<String, Any>) {
         private const val ASSURANCE_LEVEL = "helseid://claims/identity/assurance_level"
         private const val SECURITY_LEVEL = "helseid://claims/identity/security_level"
         private const val HPR_NUMBER = "helseid://claims/hpr/hpr_number"
+        private const val HPR_DETAILS = "helseid://claims/hpr/details"
         private const val APPROVALS = "approvals"
         private const val PROFESSION = "profession"
         private const val AUTHORIZATION = "authorization"
         private const val REQUISITION_RIGHTS = "requisition_rights"
         private const val SPECIALITIES = "specialities"
-        private const val HPR_DETAILS = "helseid://claims/hpr/details"
         private const val VALUE = "value"
         private const val DESCRIPTION = "description"
 
