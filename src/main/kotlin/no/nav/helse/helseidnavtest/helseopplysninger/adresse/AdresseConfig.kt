8641797fbffb6feafa6d608375341d40410012e4
Jan-Olav Eide <jan-olaveide@Jan-Olav-sin-MacBook-Pro.local>
2024-03-13 11:32:43 +0100

    disable

--- src/main/kotlin/no/nav/helse/helseidnavtest/helseopplysninger/adresse/AdresseConfig.kt
+++ src/main/kotlin/no/nav/helse/helseidnavtest/helseopplysninger/adresse/AdresseConfig.kt
@@ -3,5 +3,5 @@ package no.nav.helse.helseidnavtest.helseopplysninger.adresse
 import org.springframework.boot.context.properties.ConfigurationProperties
 import no.nav.helse.helseidnavtest.helseopplysninger.WSConfig
 
-@ConfigurationProperties("adresse")
+//@ConfigurationProperties("adresse")
 class AdresseConfig(url: String, username: String, password: String) : WSConfig(url,username,password)
