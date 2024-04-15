e9083f37d73142abc6295a079d1a0f3e6a98977a
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-04-15 14:29:47 +0200

    revert

--- src/main/kotlin/no/nav/helseidnavtest/error/ErrorHandling.kt
+++ src/main/kotlin/no/nav/helseidnavtest/error/ErrorHandling.kt
@@ -4,16 +4,17 @@ import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.*
 import org.springframework.http.client.ClientHttpResponse
+import org.springframework.web.ErrorResponseException
 import java.net.URI
 
 fun handleErrors(req: HttpRequest, res: ClientHttpResponse, detail: String) {
     throw when (res.statusCode) {
         NOT_FOUND -> NotFoundException("Fant ikke noe for $detail", req.uri)
-        else -> RecoverableException("Fikk response ${res.statusCode}", req.uri)
+        else -> RecoverableException("Fikk response ${res.statusCode}", req.uri,BAD_REQUEST)
     }
 }
 
-abstract class IrrecoverableGraphQLException(val status : HttpStatus, msg : String?, uri: URI, cause: Throwable? = null) : IrrecoverableException("$msg (${status.value()})", uri, cause) {
+abstract class IrrecoverableGraphQLException(val status : HttpStatus, msg : String?, uri: URI, cause: Throwable? = null) : IrrecoverableException(msg, uri,status, cause) {
 
     class NotFoundGraphQLException(msg : String?, uri: URI,cause: Throwable? = null) : IrrecoverableGraphQLException(NOT_FOUND, msg,uri,cause)
     class BadGraphQLException(msg : String?,  uri: URI,cause: Throwable? = null) : IrrecoverableGraphQLException(BAD_REQUEST, msg,uri,cause)
@@ -21,10 +22,10 @@ abstract class IrrecoverableGraphQLException(val status : HttpStatus, msg : Stri
     class UnauthorizedGraphQLException(msg : String, uri: URI,cause: Throwable? =null) : IrrecoverableGraphQLException(UNAUTHORIZED, msg,uri,cause)
 }
 
-abstract class RecoverableGraphQLException(msg : String?, uri: URI,cause: Throwable? = null) : RecoverableException(msg,uri,cause) {
-    class UnhandledGraphQLException(msg : String, uri: URI,cause: Throwable? = null) : RecoverableGraphQLException(msg, uri,cause)
+abstract class RecoverableGraphQLException(msg : String?, uri: URI,status : HttpStatus,cause: Throwable? = null) : RecoverableException(msg,uri,status,cause) {
+    class UnhandledGraphQLException(msg : String, uri: URI,cause: Throwable? = null) : RecoverableGraphQLException(msg, uri,BAD_REQUEST,cause)
 }
 
-class NotFoundException(msg: String?, uri: URI, cause: Throwable? = null) : IrrecoverableException(msg, uri, cause)
-open class IrrecoverableException(msg: String?, uri: URI? = null, cause: Throwable? = null) :RuntimeException("$uri $msg", cause)
-open class RecoverableException(msg: String?, uri: URI? = null,cause: Throwable? = null) : RuntimeException("$uri $msg", cause)
+class NotFoundException(msg: String?, uri: URI, cause: Throwable? = null) : IrrecoverableException(msg, uri,NOT_FOUND, cause)
+open class IrrecoverableException(msg: String?, uri: URI? = null, status : HttpStatus,cause: Throwable? = null) : ErrorResponseException(status, cause)
+open class RecoverableException(msg: String?, uri: URI? = null,status : HttpStatus,cause: Throwable? = null): ErrorResponseException(status, cause)
