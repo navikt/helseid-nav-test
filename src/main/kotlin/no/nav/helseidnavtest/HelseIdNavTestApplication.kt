064de7646422d9d83f3d13afdba07576a444166f
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-04-15 10:53:34 +0200

    exceptions

--- src/main/kotlin/no/nav/helseidnavtest/HelseIdNavTestApplication.kt
+++ src/main/kotlin/no/nav/helseidnavtest/HelseIdNavTestApplication.kt
@@ -6,9 +6,11 @@ import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableWebMvc
 @ConfigurationPropertiesScan
 @EnableRetry
 class HelseIdNavTestApplication
