7a1a8c2aa086fa2bd3815e51e0591b7757e5e40c
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-04-15 12:06:28 +0200

    exceptions

--- src/main/kotlin/no/nav/helseidnavtest/oppslag/person/PDLController.kt
+++ src/main/kotlin/no/nav/helseidnavtest/oppslag/person/PDLController.kt
@@ -1,10 +1,6 @@
 package no.nav.helseidnavtest.oppslag.person
-import no.nav.helseidnavtest.error.IrrecoverableGraphQLException.NotFoundGraphQLException
 import no.nav.helseidnavtest.oppslag.arbeid.Fødselsnummer
 import no.nav.helseidnavtest.oppslag.person.PDLConfig.Companion.PDL
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON
 import org.springframework.http.ResponseEntity.status
 import org.springframework.web.ErrorResponseException
@@ -12,7 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.context.request.NativeWebRequest
 
 @RestController(PDL)
 class PDLController(private val pdl: PDLClient) {
@@ -21,11 +16,11 @@ class PDLController(private val pdl: PDLClient) {
 
     @GetMapping("/$PDL") fun navn(@RequestParam fnr: Fødselsnummer) = pdl.navn(fnr)
 
-    @ExceptionHandler(NotFoundGraphQLException::class)
-    fun notFound(e: NotFoundGraphQLException) = createProblem(e, NOT_FOUND)}
+    @ExceptionHandler(ErrorResponseException::class)
+    fun problem(e: ErrorResponseException) = createProblem(e)}
 
-private fun createProblem(e: ErrorResponseException, status: HttpStatus) =
-    status(status)
-        .headers(HttpHeaders().apply { contentType = APPLICATION_PROBLEM_JSON })
+private fun createProblem(e: ErrorResponseException) =
+    status(e.statusCode)
+        .headers(e.headers.apply { contentType = APPLICATION_PROBLEM_JSON })
         .body(e.body)
 
