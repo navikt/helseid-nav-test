bee7b74446c52f2fb7159248f1c3295caa0f177d
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-04-16 08:46:58 +0200

    revert

--- src/main/kotlin/no/nav/helseidnavtest/error/ErrorHandling.kt
+++ src/main/kotlin/no/nav/helseidnavtest/error/ErrorHandling.kt
@@ -2,8 +2,8 @@ package no.nav.helseidnavtest.error
 
 import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.*
-import org.springframework.http.ProblemDetail.*
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.ProblemDetail.forStatusAndDetail
 import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.ErrorResponseException
 import java.net.URI
@@ -11,12 +11,16 @@ import java.net.URI
 fun handleErrors(req: HttpRequest, res: ClientHttpResponse, detail: String): Nothing =
     throw when (res.statusCode) {
         NOT_FOUND -> NotFoundException(detail = detail, uri = req.uri)
-        else -> RecoverableException(BAD_REQUEST, "Fikk response ${res.statusCode}", req.uri)
+        else -> RecoverableException(res.statusCode as HttpStatus, "Fikk response ${res.statusCode}", req.uri)
     }
 
 class NotFoundException(title: String? = "Ikke funnet", detail: String, uri: URI, cause: Throwable? = null) : IrrecoverableException(NOT_FOUND, title, detail, uri, cause)
-open class IrrecoverableException(status: HttpStatus, title: String? = null, detail: String, uri: URI? = null, cause: Throwable? = null) :
-   ErrorResponseException(status, forStatusAndDetail(status,detail).apply { this.title=title
-        properties = mapOf("uri" to "$uri") },  cause)
-open class RecoverableException(status: HttpStatus, detail: String, uri: URI? = null, cause: Throwable? = null):ErrorResponseException(status, forStatusAndDetail(status,detail).apply { title=detail
-    properties = mapOf("uri" to "$uri") }, cause)
+open class IrrecoverableException(status: HttpStatus, title: String? = null, detail: String, uri: URI? = null, cause: Throwable? = null) : ErrorResponseException(status, problemDetail(status, title, detail, uri),  cause)
+open class RecoverableException(status: HttpStatus, detail: String, uri: URI? = null, cause: Throwable? = null): ErrorResponseException(status,problemDetail(status, "title", detail,uri
+), cause)
+
+private fun problemDetail(status: HttpStatus, title: String?, detail: String, uri: URI?) =
+    forStatusAndDetail(status, detail).apply {
+        this.title = title
+        properties = mapOf("uri" to "$uri")
+}
