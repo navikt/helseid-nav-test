e54ea311ce21ab0ea50f215d33113057e0e063e7
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-04-16 13:01:12 +0200

    revert

--- src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingGenerator.kt
+++ src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingGenerator.kt
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus.*
 import org.springframework.retry.annotation.Retryable
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -21,56 +22,76 @@ class DialogmeldingGenerator(private val pdl: PDLClient) {
     private val log = getLogger(DialogmeldingGenerator::class.java)
 
     @Retryable(retryFor = [RecoverableException::class])
-    fun genererDialogmelding(pasient: Fødselsnummer) : String {
+    fun genererDialogmelding(pasient: Fødselsnummer) =
         when (val a = SecurityContextHolder.getContext().authentication) {
-        is OAuth2AuthenticationToken -> {
-            log.info(a.toString())
-            //a.principal.attributes.forEach { (k, v) -> log.info("key: $k, value: $v") }
-            val kontor = BehandlerKontor(
-                    partnerId = PartnerId(123456789),
-                    navn = "Et legekontor",
-                    orgnummer = Virksomhetsnummer("123456789"),
-                    postnummer = "1234",
-                    poststed = "Oslo",
-                    adresse = "Fyrstikkalleen 1",
-                    herId = 12345678)
-
-            val behandlerNavn = a.navn()
-            val behandler =  Behandler(
-                    UUID.randomUUID(),
-                    fornavn = behandlerNavn.fornavn,
-                    mellomnavn =behandlerNavn.mellomnavn,
-                    etternavn = behandlerNavn.etternavn,
-                    herId = 123456789,
-                    hprId = 987654321,
-                    telefon = "12345678",
-                    personident = Personident("12345678901"),
-                    kontor = kontor)
-
-            val bestilling = DialogmeldingBestilling(uuid = UUID.randomUUID(),
-                    behandler = behandler,
-                    arbeidstakerPersonident =  Personident("01010111111"),
-                    parentRef = "parent ref",
-                    conversationUuid =  UUID.randomUUID(),
-                    tekst = "dette er litt tekst",
-                    vedlegg = ClassPathResource("test.pdf").inputStream.readBytes(),
-                )
-            with(pdl.navn(pasient)) {
-                val arbeidstaker = Arbeidstaker(Personident(pasient.fnr), fornavn, mellomnavn, etternavn)
-                return opprettDialogmelding(bestilling, arbeidstaker).message.also {
+            is OAuth2AuthenticationToken -> {
+                log.info(a.toString())
+                //a.principal.attributes.forEach { (k, v) -> log.info("key: $k, value: $v") }
+                val behandler = behandler(a.navn(),a.herid(), behandlerKontor())
+                val bestilling = bestilling(behandler)
+                val arbeidstaker = arbeidstaker(pasient)
+                opprettDialogmelding(bestilling, arbeidstaker).message.also {
                     log.trace("XML {}", this)
                 }
             }
+            else -> {
+                log.error("Uventet type {}", a::class.java)
+                throw IrrecoverableException(INTERNAL_SERVER_ERROR,"Uventet type", "${a::class.java}")
+            }
         }
-        else -> {
-            log.error("Uventet type {}", a::class.java)
-            throw IrrecoverableException(INTERNAL_SERVER_ERROR,"Uventet type", "${a::class.java}")
+
+
+    private fun arbeidstaker(pasient: Fødselsnummer) =
+        with(pdl.navn(pasient)) {
+            Arbeidstaker(Personident(pasient.fnr), fornavn, mellomnavn, etternavn)
         }
-    }
-    }
+
+    private fun bestilling(behandler: Behandler) =
+        DialogmeldingBestilling(
+            UUID.randomUUID(),
+            behandler,
+            Personident("01010111111"),
+            "parent ref",
+            UUID.randomUUID(),
+            tekst = "dette er litt tekst",
+            vedlegg = ClassPathResource("test.pdf").inputStream.readBytes(),
+        )
+
+    private fun behandler(
+        behandlerNavn: Navn,
+        herid: Int,
+        kontor: BehandlerKontor
+    ) = Behandler(
+        UUID.randomUUID(),
+        fornavn = behandlerNavn.fornavn,
+        mellomnavn = behandlerNavn.mellomnavn,
+        etternavn = behandlerNavn.etternavn,
+        herId = herid,
+        hprId = 987654321,
+        telefon = "12345678",
+        personident = Personident("12345678901"),
+        kontor = kontor
+    )
+
+    private fun behandlerKontor() = BehandlerKontor(
+        partnerId = PartnerId(123456789),
+        navn = "Et legekontor",
+        orgnummer = Virksomhetsnummer("123456789"),
+        postnummer = "1234",
+        poststed = "Oslo",
+        adresse = "Fyrstikkalleen 1",
+        herId = 12345678
+    )
 }
+private fun OAuth2AuthenticationToken.attribute(a: String) = principal.attribute(a)
+
+private fun OAuth2AuthenticationToken.herid() = attribute("helseid://claims/her/her_id").toInt()
+
 
 private fun OAuth2AuthenticationToken.navn() =
     with(principal)  {
-        Navn(getAttribute("given_name")!!, getAttribute("middle_name"), getAttribute("family_name")!!)
+        Navn(attribute("given_name"), attribute("middle_name"), attribute("family_name"))
     }
+
+private fun OAuth2User.attribute(a: String) =
+   getAttribute<String>(a) ?: throw IrrecoverableException(INTERNAL_SERVER_ERROR, "Mangler attributt", a)
