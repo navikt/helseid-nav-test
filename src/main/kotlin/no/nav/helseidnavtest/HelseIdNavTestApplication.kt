b6715f7edffb9694927196d85ac408ff68656a48
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-05-06 11:58:09 +0200

    b27apvl220.preprod.local

--- src/main/kotlin/no/nav/helseidnavtest/HelseIdNavTestApplication.kt
+++ src/main/kotlin/no/nav/helseidnavtest/HelseIdNavTestApplication.kt
@@ -1,5 +1,8 @@
 package no.nav.helseidnavtest
 import no.nav.boot.conditionals.Cluster.Companion.profiler
+import no.nav.helseidnavtest.oppslag.person.PDLController
+import org.slf4j.LoggerFactory.getLogger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
@@ -17,8 +20,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 @EnableJms
 class HelseIdNavTestApplication
 
+@Value("\${helseid.emottak.password}")
+lateinit var pw: String
+
+private val log = getLogger(HelseIdNavTestApplication::class.java)
+
+
 fun main(args: Array<String>) {
     runApplication<HelseIdNavTestApplication>(*args) {
+        log.info("XXXXXX: $pw")
         setAdditionalProfiles(*profiler())
     }
 }
--- src/main/kotlin/no/nav/helseidnavtest/dialogmelding/ApprecReceiver.kt
+++ src/main/kotlin/no/nav/helseidnavtest/dialogmelding/ApprecReceiver.kt
@@ -3,11 +3,13 @@ package no.nav.helseidnavtest.dialogmelding
 import no.nav.helseidnavtest.dialogmelding.ApprecReceiver
 import no.nav.helseopplysninger.apprec.XMLAppRec
 import org.slf4j.LoggerFactory.getLogger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
 
 @Component
 internal class ApprecReceiver {
+
     private val log = getLogger(ApprecReceiver::class.java)
     @JmsListener(destination = "QA.Q1_HELSEID.IU03_UTSENDING_REPLY")
     fun receiveMessage(apprec: XMLAppRec) {
