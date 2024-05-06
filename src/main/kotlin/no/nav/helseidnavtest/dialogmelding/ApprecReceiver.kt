c47aca80ba693850b8c8cff1c9c46277d742469f
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-05-06 12:25:27 +0200

    b27apvl220.preprod.local

--- src/main/kotlin/no/nav/helseidnavtest/dialogmelding/ApprecReceiver.kt
+++ src/main/kotlin/no/nav/helseidnavtest/dialogmelding/ApprecReceiver.kt
@@ -9,11 +9,8 @@ import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
 
 @Component
-internal class ApprecReceiver(@Value("\${helseid.emottak.password}") private val pw: String) {
+internal class ApprecReceiver {
     private val log = getLogger(ApprecReceiver::class.java)
-    init {
-        log.info("XXXXXX: $pw")
-    }
     @JmsListener(destination = "QA.Q1_HELSEID.IU03_UTSENDING_REPLY")
     fun receiveMessage(apprec: XMLAppRec) {
         log.info("Received <$apprec>")
