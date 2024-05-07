8df7cae0f0942f6a2250104a82985d8635d3348b
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-05-07 09:55:47 +0200

    dialogmelding.host

--- src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingConfig.kt
+++ src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingConfig.kt
@@ -1,13 +1,10 @@
 package no.nav.helseidnavtest.dialogmelding
 
 import no.nav.helseidnavtest.dialogmelding.DialogmeldingConfig.Companion.DIALOGMELDING
-import java.net.URI
 import org.springframework.boot.context.properties.ConfigurationProperties
-import no.nav.helseidnavtest.oppslag.AbstractRestConfig
-import no.nav.helseidnavtest.oppslag.person.PDLConfig.Companion.PDL
 
 @ConfigurationProperties(DIALOGMELDING)
-class DialogmeldingConfig(val request: String,val response: String, val host: String)  {
+class DialogmeldingConfig(val request: String,val response: String, val host: String, val channel: String, val qm: String, val port: Int = 1413)  {
 
     override fun toString() = "$javaClass.simpleName [request=$request, response=$response]"
 
