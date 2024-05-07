2d1af57d63cbcf66401293c9b9d20b58c68ff042
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-05-07 10:13:21 +0200

    dialogmelding.host

--- src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingConfig.kt
+++ src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingConfig.kt
@@ -4,9 +4,9 @@ import no.nav.helseidnavtest.dialogmelding.DialogmeldingConfig.Companion.DIALOGM
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(DIALOGMELDING)
-class DialogmeldingConfig(val request: String,val response: String, val host: String = "mqls02.preprod.local", val channel: String, val qm: String, val port: Int = 1413)  {
+class DialogmeldingConfig(val request: String,val reply: String, val host: String = "mqls02.preprod.local", val channel: String, val qm: String, val port: Int = 1413)  {
 
-    override fun toString() = "$javaClass.simpleName [request=$request, response=$response]"
+    override fun toString() = "$javaClass.simpleName [request=$request, reply=$reply]"
 
     companion object {
         const val DIALOGMELDING = "dialogmelding"
