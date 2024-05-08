ab867159423d4c8284a45956a70a7107bb89dbc3
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-05-08 08:29:15 +0200

    partneroppslag

--- src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingConfig.kt
+++ src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingConfig.kt
@@ -5,7 +5,7 @@ import no.nav.helseidnavtest.oppslag.WSConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 import java.net.URI
 
-@ConfigurationProperties(prefix = DIALOGMELDING, ignoreInvalidFields = true)
+@ConfigurationProperties(DIALOGMELDING)
 data class DialogmeldingConfig(val uri: URI, val request: String, val reply: String) : WSConfig(uri) {
 
     companion object {
