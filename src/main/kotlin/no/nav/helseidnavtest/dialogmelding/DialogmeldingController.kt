4d864bd354da075786294274973db3b446cd9069
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-05-31 07:59:02 +0200

    avtale

--- src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingController.kt
+++ src/main/kotlin/no/nav/helseidnavtest/dialogmelding/DialogmeldingController.kt
@@ -1,12 +1,13 @@
 package no.nav.helseidnavtest.dialogmelding
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/xml")
 class DialogmeldingController(private val sender: DialogmeldingSender, private val generator: DialogmeldingGenerator, private val emottak: DialogmeldingRestAdapter) {
 
     @GetMapping(value = ["/generer"])
-    fun generer(@RequestParam pasient: Fødselsnummer) = generator.genererDialogmelding(pasient)
+    fun generer(@RequestParam pasient: Fødselsnummer) = generator.genererDialogmelding(pasient, UUID.randomUUID())
 
     @GetMapping(value = ["/send"])
     fun send(@RequestParam pasient: Fødselsnummer) = sender.send(pasient)
