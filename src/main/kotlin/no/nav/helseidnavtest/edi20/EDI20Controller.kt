84d79524b548b6c09e1d95e201794002b402feb5
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-07-04 18:25:21 +0200

    token claims wins if duplicates

--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
@@ -1,5 +1,6 @@
 package no.nav.helseidnavtest.edi20
 import jakarta.xml.bind.Marshaller
+import jakarta.xml.bind.Marshaller.*
 import no.nav.helseidnavtest.dialogmelding.DialogmeldingGenerator
 import no.nav.helseidnavtest.dialogmelding.Fødselsnummer
 import no.nav.helseidnavtest.edi20.EDI20Config.Companion.EDI20
@@ -24,23 +25,30 @@ class EDI20Controller(private val a: EDI20RestClientAdapter, private val generat
     @GetMapping("/messages") fun messages() = a.messages()
 
     @GetMapping("/dialogmelding") fun dialogmelding(@RequestParam pasient: Fødselsnummer): String {
-        log.trace("Genererer melding")
-        val msg = generator.genererDialogmelding(pasient, UUID.randomUUID())
-        log.trace("Generert melding OK")
-        val writer = StringWriter()
-        log.trace("Marshalling XML")
-        Jaxb2Marshaller().apply {
-            setMarshallerProperties(mapOf(Marshaller.JAXB_FORMATTED_OUTPUT to true))
-            setClassesToBeBound(
-                XMLEIFellesformat::class.java,
-                XMLBase64Container::class.java,
-                XMLDialogmelding::class.java,
-                XMLMsgHead::class.java)
-        }.createMarshaller().marshal(msg, writer)
-        log.trace("XML {}", writer.toString())
-        val b64 = Base64.getEncoder().encodeToString(writer.toString().toByteArray())
-        val bd = EDI20DTOs.BusinessDocument(b64, EDI20DTOs.Properties(EDI20DTOs.System("HelseIdNavTest", "1.0.0")))
-        a.postMessage(bd)
-        return "OK"
+        try {
+            log.trace("Genererer melding")
+            val msg = generator.genererDialogmelding(pasient, UUID.randomUUID())
+            log.trace("Generert melding OK")
+            val writer = StringWriter()
+            log.trace("Marshalling XML")
+            Jaxb2Marshaller().apply {
+                setMarshallerProperties(mapOf(JAXB_FORMATTED_OUTPUT to true))
+                setClassesToBeBound(
+                    XMLEIFellesformat::class.java,
+                    XMLBase64Container::class.java,
+                    XMLDialogmelding::class.java,
+                    XMLMsgHead::class.java)
+            }.createMarshaller().marshal(msg, writer)
+            log.trace("XML {}", writer.toString())
+            val b64 = Base64.getEncoder().encodeToString(writer.toString().toByteArray())
+            val bd = EDI20DTOs.BusinessDocument(b64, EDI20DTOs.Properties(EDI20DTOs.System("HelseIdNavTest", "1.0.0")))
+            a.postMessage(bd)
+            return "OK"
+        }
+        catch (e: Exception) {
+            log.error("Feil ved generering av dialogmelding", e)
+            return "NOT OK"
+        }
+
     }
 }
