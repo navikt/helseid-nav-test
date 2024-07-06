36bd29a7d7180fdbe5e9dc04c392523257369d72
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-07-06 14:54:23 +0200

    test

--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
@@ -14,7 +14,7 @@ class EDI20Controller(private val a: EDI20Service) {
 
     private val log = getLogger(EDI20Controller::class.java)
 
-    @GetMapping("/messages") fun messages(@Parameter(schema = Schema(allowableValues = arrayOf("8142519","8142519"))) @RequestParam herId: String) = a.poll(HerId(herId))
+    @GetMapping("/messages") fun messages(@Parameter(schema = Schema(allowableValues = arrayOf("8142519","8142520"))) @RequestParam herId: String) = a.poll(HerId(herId))
 
     @GetMapping("/dialogmelding") fun dialogmelding(@RequestParam pasient: Fødselsnummer): String {
         runCatching {
