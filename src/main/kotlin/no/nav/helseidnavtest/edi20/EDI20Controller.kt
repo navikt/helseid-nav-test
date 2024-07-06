b2a16641ec6291b104b385a007a7df48a10e78c8
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-07-06 21:52:25 +0200

    set sender and receiver dynamically

--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
@@ -7,14 +7,11 @@ import no.nav.helseidnavtest.edi20.EDI20Config.Companion.EDI20
 import no.nav.helseidnavtest.edi20.EDI20Config.Companion.EDI2_ID
 import no.nav.helseidnavtest.edi20.EDI20Config.Companion.EDI1_ID
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.UUID
 
 @RestController(EDI20)
+@RequestMapping("/edi20")
 class EDI20Controller(private val a: EDI20Service) {
 
     private val log = getLogger(EDI20Controller::class.java)
