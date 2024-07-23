9a28b52768ecfbc5895d948e19b26cbe666a75e3
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-07-23 19:02:56 +0200

    deft

--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20DeftRestClientAdapter.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20DeftRestClientAdapter.kt
@@ -8,18 +8,12 @@ import org.springframework.stereotype.Component
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.client.RestClient
 import org.springframework.web.multipart.MultipartFile
-import java.net.URI
 
 @Component
 class EDI20DeftRestClientAdapter(@Qualifier(EDI20DEFT) restClient: RestClient, private val cf: EDI20DeftConfig) : AbstractRestClientAdapter(restClient,cf) {
 
-    fun upload(file: MultipartFile, herId: String): URI? {
-
-         val body = LinkedMultiValueMap<String, Any>().apply {
-            add("file", file.resource)
-        }
-
-        return restClient
+    fun upload(file: MultipartFile, herId: String) =
+        restClient
             .post()
             .uri { cf.uploadURI(it,herId) }
             .headers {
@@ -29,11 +23,13 @@ class EDI20DeftRestClientAdapter(@Qualifier(EDI20DEFT) restClient: RestClient, p
                     .build()
                 it.herIdHeader(herId)
             }
-            .body(body)
+            .body(LinkedMultiValueMap<String, Any>().apply {
+                add("file", file.resource)
+            })
             .retrieve()
             .toBodilessEntity()
             .headers.location
-    }
+
     override fun toString() =
         "${javaClass.simpleName} [restClient=$restClient, cfg=$cfg]"
 
