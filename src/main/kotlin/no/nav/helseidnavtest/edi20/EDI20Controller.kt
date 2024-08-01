5ddd6e3b817c6bf409726698aa3868ebec2d68ee
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-08-01 12:53:34 +0200

    error handling

--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20Controller.kt
@@ -1,7 +1,6 @@
 package no.nav.helseidnavtest.edi20
 
-
-import no.nav.helseidnavtest.dialogmelding.HerId.Companion.of
+import no.nav.helseidnavtest.dialogmelding.HerId
 import no.nav.helseidnavtest.edi20.EDI20Config.Companion.DOK_PATH
 import no.nav.helseidnavtest.edi20.EDI20Config.Companion.EDI20
 import no.nav.helseidnavtest.edi20.EDI20Config.Companion.MESSAGES_PATH
@@ -9,27 +8,26 @@ import org.springframework.http.MediaType.APPLICATION_XML_VALUE
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
-
 @RestController(EDI20)
 @RequestMapping("/$EDI20/")
 class EDI20Controller(private val edi: EDI20Service) {
 
-    @PostMapping("${DOK_PATH}/Apprec")
-    fun apprec(@Herid @RequestParam herId: String, @PathVariable id: UUID) = edi.apprec(of(herId), id)
+    @PostMapping("${DOK_PATH}/apprec")
+    fun apprec(@Herid @RequestParam herId: HerId, @PathVariable id: UUID) = edi.apprec(herId, id)
 
     @GetMapping(MESSAGES_PATH)
-    fun poll(@Herid @RequestParam herId: String, @RequestParam(defaultValue = "false") apprec: Boolean) =
-        edi.poll(of(herId), apprec)
+    fun poll(@Herid @RequestParam herId: HerId, @RequestParam(defaultValue = "false") apprec: Boolean) =
+        edi.poll(herId, apprec)
 
     @PostMapping(MESSAGES_PATH)
-    fun send(@Herid @RequestParam herId: String) = edi.send(of(herId))
+    fun send(@Herid @RequestParam herId: HerId) = edi.send(herId)
 
     @PutMapping("${DOK_PATH}/read/{herId}")
-    fun lest(@Herid @PathVariable herId: String, @PathVariable id: UUID) = edi.lest(of(herId), id)
+    fun lest(@Herid @PathVariable herId: HerId, @PathVariable id: UUID) = edi.lest(herId, id)
 
     @GetMapping(DOK_PATH, produces = [APPLICATION_XML_VALUE])
-    fun les(@Herid herId: String, @PathVariable id: UUID) = edi.les(of(herId), id)
+    fun les(@Herid herId: HerId, @PathVariable id: UUID) = edi.les(herId, id)
 
     @GetMapping("${DOK_PATH}/status")
-    fun status(@Herid herId: String, @PathVariable id: UUID) = edi.status(of(herId), id)
+    fun status(@Herid herId: HerId, @PathVariable id: UUID) = edi.status(herId, id)
 }
--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20DeftController.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20DeftController.kt
@@ -1,7 +1,6 @@
 package no.nav.helseidnavtest.edi20
 
 import no.nav.helseidnavtest.dialogmelding.HerId
-import no.nav.helseidnavtest.dialogmelding.HerId.Companion.of
 import no.nav.helseidnavtest.edi20.EDI20DeftConfig.Companion.EDI20DEFT
 import no.nav.helseidnavtest.edi20.EDI20DeftConfig.Companion.OBJECT_PATH
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
@@ -18,22 +17,22 @@ class EDI20DeftController(private val deft: EDI20DeftService) {
         deft.upload(file, herId)
 
     @GetMapping("$OBJECT_PATH/{key}/status")
-    fun status(key: String, @Herid @RequestParam herId: String) =
-        deft.status(key, of(herId))
+    fun status(key: String, @Herid @RequestParam herId: HerId) =
+        deft.status(key, herId)
 
     @GetMapping(OBJECT_PATH)
-    fun les(uri: URI, @Herid @RequestParam herId: String) =
-        deft.les(uri, of(herId))
+    fun les(uri: URI, @Herid @RequestParam herId: HerId) =
+        deft.les(uri, herId)
 
     @DeleteMapping(OBJECT_PATH)
-    fun slett(uri: URI, @Herid @RequestParam herId: String) =
-        deft.slett(uri, of(herId))
+    fun slett(uri: URI, @Herid @RequestParam herId: HerId) =
+        deft.slett(uri, herId)
 
     @DeleteMapping("$OBJECT_PATH/{key}")
-    fun slett(@PathVariable key: String, @Herid @RequestParam herId: String) =
-        deft.slett(key, of(herId))
+    fun slett(@PathVariable key: String, @Herid @RequestParam herId: HerId) =
+        deft.slett(key, herId)
 
     @PutMapping("$OBJECT_PATH/{key}")
-    fun kvitter(@PathVariable key: String, @Herid @RequestParam herId: String) =
-        deft.kvitter(key, of(herId))
+    fun kvitter(@PathVariable key: String, @Herid @RequestParam herId: HerId) =
+        deft.kvitter(key, herId)
 }
