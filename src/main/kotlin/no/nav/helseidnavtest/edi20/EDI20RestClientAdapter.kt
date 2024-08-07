ad3477a5cdf14c6140bc4b21544adad0c638dc22
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-08-07 13:43:26 +0200

    error handler

--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20RestClientAdapter.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20RestClientAdapter.kt
@@ -60,7 +60,7 @@ class EDI20RestClientAdapter(
             .headers { it.herId(herId) }
             .accept(APPLICATION_JSON)
             .retrieve()
-            .onStatus({ it.isError }) { req, res -> handler.handle(req, res) }
+            //   .onStatus({ it.isError }) { req, res -> handler.handle(req, res) }
             .body<List<Meldinger>>()
 
     fun send(herId: HerId, hodemelding: String) = restClient
