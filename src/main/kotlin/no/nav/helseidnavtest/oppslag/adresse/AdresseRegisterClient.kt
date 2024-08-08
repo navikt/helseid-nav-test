f0721efedb1e67583aa823e68d702f1994ef3023
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-08-08 14:54:34 +0200

    localtion

--- src/main/kotlin/no/nav/helseidnavtest/oppslag/adresse/AdresseRegisterClient.kt
+++ src/main/kotlin/no/nav/helseidnavtest/oppslag/adresse/AdresseRegisterClient.kt
@@ -2,14 +2,11 @@ package no.nav.helseidnavtest.oppslag.adresse
 
 import no.nav.helseidnavtest.dialogmelding.HerId
 import no.nav.helseidnavtest.dialogmelding.Orgnummer
-import no.nav.helseidnavtest.error.RecoverableException
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.retry.annotation.Recover
-import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 
 @Service
-@Retryable(include = [RecoverableException::class])
+//@Retryable(include = [RecoverableException::class])
 class AdresseRegisterClient(private val adapter: AdresseRegisterCXFAdapter) {
 
     private val log = getLogger(AdresseRegisterClient::class.java)
@@ -20,8 +17,14 @@ class AdresseRegisterClient(private val adapter: AdresseRegisterCXFAdapter) {
 
     fun detajer(id: HerId) = adapter.detaljer(id)
 
+    /*
     @Recover
     fun herIdForOrgnummer(e: Exception, nummer: Orgnummer): HerId = throw e.also {
         log.error("Recoverable exception feilet for oppslag på orgnummer {}", nummer, it)
     }
+
+    @Recover
+    fun detajer(e: Exception, id: HerId) = throw e.also {
+        log.error("Recoverable exception feilet for oppslag på herid {}", id, it)
+    }*/
 }
