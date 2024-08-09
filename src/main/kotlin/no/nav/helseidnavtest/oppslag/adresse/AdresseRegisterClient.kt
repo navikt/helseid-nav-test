0d917b7902203da47e67aa0258de7cf038602a76
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-08-09 18:29:21 +0200

    caching, slå opp navn nivå 1

--- src/main/kotlin/no/nav/helseidnavtest/oppslag/adresse/AdresseRegisterClient.kt
+++ src/main/kotlin/no/nav/helseidnavtest/oppslag/adresse/AdresseRegisterClient.kt
@@ -2,12 +2,15 @@ package no.nav.helseidnavtest.oppslag.adresse
 
 import no.nav.helseidnavtest.dialogmelding.HerId
 import no.nav.helseidnavtest.dialogmelding.Orgnummer
+import no.nav.helseidnavtest.error.RecoverableException
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.retry.annotation.Recover
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 
 @Service
-//@Retryable(include = [RecoverableException::class])
+@Retryable(include = [RecoverableException::class])
 class AdresseRegisterClient(private val adapter: AdresseRegisterCXFAdapter) {
 
     private val log = getLogger(AdresseRegisterClient::class.java)
@@ -16,15 +19,15 @@ class AdresseRegisterClient(private val adapter: AdresseRegisterCXFAdapter) {
 
     @Cacheable("ardetails")
     fun navn(id: HerId) = adapter.navn(id)
-    
-    /*
+
+    @Throws(Exception::class)
     @Recover
     fun herIdForOrgnummer(e: Exception, nummer: Orgnummer): HerId = throw e.also {
         log.error("Recoverable exception feilet for oppslag på orgnummer {}", nummer, it)
     }
 
     @Recover
-    fun detajer(e: Exception, id: HerId) = throw e.also {
-        log.error("Recoverable exception feilet for oppslag på herid {}", id, it)
-    }*/
+    fun navn(e: Exception, id: HerId): Pair<String, String> = throw e.also {
+        log.error("Recoverable exception feilet for navn oppslag på herid {}", id, it)
+    }
 }
