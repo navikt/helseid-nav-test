e821981a57a5b00a678f098679dcb1845e95ae6b
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-08-20 16:45:16 +0200

    lag lege

--- src/main/kotlin/no/nav/helseidnavtest/oppslag/adresse/AdresseRegisterController.kt
+++ src/main/kotlin/no/nav/helseidnavtest/oppslag/adresse/AdresseRegisterController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("adresse")
+@RestController
 class AdresseRegisterController(private val adresse: AdresseRegisterClient) {
 
     @GetMapping("/part")
