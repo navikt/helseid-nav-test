342b283f175ce1ea47a323ef2f7b8344b28f67a8
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-08-22 14:59:05 +0200

    reorg

--- src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20DialogmeldingGenerator.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/EDI20DialogmeldingGenerator.kt
@@ -8,7 +8,6 @@ import no.nav.helseidnavtest.oppslag.adresse.Bestilling
 import no.nav.helseidnavtest.oppslag.person.PDLClient
 import no.nav.helseopplysninger.apprec.XMLAppRec
 import no.nav.helseopplysninger.hodemelding.XMLMsgHead
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.oxm.jaxb.Jaxb2Marshaller
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
@@ -22,31 +21,25 @@ class EDI20DialogmeldingGenerator(
     private val mapper: EDI20DialogmeldingMapper,
     private val pdl: PDLClient) {
 
-    private val log = getLogger(EDI20DialogmeldingGenerator::class.java)
-
     fun marshal(fra: HerId, til: HerId, pasient: Fødselsnummer, vedlegg: Pair<URI, String>?) =
-        StringWriter().let {
-            marshaller.createMarshaller()
-                .marshal(mapper.hodemelding(Bestilling(adresse.kommunikasjonsParter(fra, til),
-                    pasient(pasient)),
-                    vedlegg),
-                    it)
-            "$it"
-        }.also { log.run { info("Marshalled: $it") } }
-
-    fun marshal(fra: HerId, til: HerId, pasient: Fødselsnummer, vedlegg: MultipartFile?) =
-        StringWriter().let {
+        StringWriter().run {
             marshaller.createMarshaller()
                 .marshal(mapper.hodemelding(Bestilling(adresse.kommunikasjonsParter(fra, til), pasient(pasient)),
-                    vedlegg),
-                    it)
-            "$it"
-        }.also { log.info("Marshalled: $it") }
+                    vedlegg), this)
+            toString()
+        }
+
+    fun marshal(fra: HerId, til: HerId, pasient: Fødselsnummer, vedlegg: MultipartFile?) =
+        StringWriter().run {
+            marshaller.createMarshaller()
+                .marshal(mapper.hodemelding(Bestilling(adresse.kommunikasjonsParter(fra, til), pasient(pasient)),
+                    vedlegg), this)
+            toString()
+        }
 
     private fun pasient(fnr: Fødselsnummer) = Pasient(fnr, pdl.navn(fnr))
 
-    fun unmarshal(apprec: String) = marshaller.createUnmarshaller().unmarshal(apprec.byteInputStream()).let {
-        log.info("Unmarshalled: $it")
+    fun unmarshal(xml: String) = marshaller.createUnmarshaller().unmarshal(xml.byteInputStream()).let {
         when (it) {
             is XMLMsgHead -> mapper.bestilling(it)
             is XMLAppRec -> mapper.apprec(it)
