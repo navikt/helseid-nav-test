bc00461c83d4da0a8604b601f4efe994d7ac1769
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-08-27 09:55:12 +0200

    inject fault

--- src/main/kotlin/no/nav/helseidnavtest/edi20/KafkaRecoverer.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/KafkaRecoverer.kt
@@ -1,10 +1,11 @@
 package no.nav.helseidnavtest.edi20
 
 import no.nav.helseidnavtest.oppslag.adresse.Bestilling
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.listener.RetryListener
+import org.springframework.retry.RetryCallback
+import org.springframework.retry.RetryContext
+import org.springframework.retry.RetryListener
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -28,17 +29,29 @@ class KafkaRecoverer(private val cfg: BestillingConfig,
 @Component
 class LoggingRetryListener : RetryListener {
     private val log = getLogger(RetryListener::class.java)
-
-    override fun failedDelivery(record: ConsumerRecord<*, *>, e: Exception, deliveryAttempt: Int) {
-        log.warn("Failed delivery of record $record on attempt $deliveryAttempt", e)
+    override fun <T : Any?, E : Throwable?> open(context: RetryContext?, callback: RetryCallback<T, E>?): Boolean {
+        log.info("open")
+        return super.open(context, callback)
     }
 
-    override fun recovered(record: ConsumerRecord<*, *>, e: Exception) {
-        log.info("Recovered record $record", e)
+    override fun <T : Any?, E : Throwable?> close(context: RetryContext?,
+                                                  callback: RetryCallback<T, E>?,
+                                                  throwable: Throwable?) {
+        log.info("close")
+        super.close(context, callback, throwable)
     }
 
-    override fun recoveryFailed(record: ConsumerRecord<*, *>, e: Exception, f: Exception) {
-        log.info("Recovery fsiled record $record", e)
+    override fun <T : Any?, E : Throwable?> onSuccess(context: RetryContext?,
+                                                      callback: RetryCallback<T, E>?,
+                                                      result: T) {
+        log.info("onSuccess")
+        super.onSuccess(context, callback, result)
     }
 
+    override fun <T : Any?, E : Throwable?> onError(context: RetryContext?,
+                                                    callback: RetryCallback<T, E>?,
+                                                    throwable: Throwable?) {
+        log.info("onError")
+        super.onError(context, callback, throwable)
+    }
 }
