d0142f3c8b3e28ec315f0aad87b1ccd2d36faffe
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-09-23 12:18:50 +0200

    htmx

--- src/main/kotlin/no/nav/helseidnavtest/HelseopplysningerController.kt
+++ src/main/kotlin/no/nav/helseidnavtest/HelseopplysningerController.kt
@@ -37,7 +37,7 @@ class MainController {
     fun clicked(model: Model): String {
         val user = SecurityContextHolder.getContext().authentication
         log.info("User: ${user.javaClass.name}")
-        model.addAttribute("tidspunkt", user.oidcUser().authenticatedAt.toString())
+        model.addAttribute("claims", user.oidcUser().claims)
         model.addAttribute("now", LocalDateTime.now().toString())
         return "clicked :: result"
     }
