3e04174e65db81cd7b3bce64d2592c050820ecf9
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-09-26 15:01:27 +0200

    ErrorHandlingDeserializer

---
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/BestillingHendelseKonsument.kt
@@ -0,0 +1,44 @@
+package no.nav.helseidnavtest.edi20
+
+import no.nav.helseidnavtest.error.IrrecoverableException
+import no.nav.helseidnavtest.oppslag.adresse.Innsending
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.DltHandler
+import org.springframework.kafka.annotation.KafkaHandler
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.annotation.RetryableTopic
+import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata
+import org.springframework.kafka.support.KafkaHeaders.DLT_EXCEPTION_MESSAGE
+import org.springframework.kafka.support.KafkaHeaders.DLT_EXCEPTION_STACKTRACE
+import org.springframework.kafka.support.KafkaMessageHeaderAccessor
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.retry.annotation.Backoff
+import org.springframework.stereotype.Component
+
+@Component
+@KafkaListener(topics = ["#{@innsendingConfig.topics.main}"], containerFactory = InnsendingConfig.INNSENDING)
+@RetryableTopic(
+    retryTopicSuffix = "retry", dltTopicSuffix = "dlt",
+    attempts = "#{@innsendingConfig.topics.retries}",
+    backoff = Backoff(delayExpression = "#{@innsendingConfig.topics.backoff}"),
+    exclude = [IrrecoverableException::class], traversingCauses = "true",
+    autoStartDltHandler = "true",
+    autoCreateTopics = "false",
+)
+class BestillingHendelseKonsument(private val edi: EDI20Service, val cfg: InnsendingConfig) {
+
+    private val log = LoggerFactory.getLogger(BestillingHendelseKonsument::class.java)
+
+    @KafkaHandler
+    fun listen(innsending: Innsending,
+               accessor: KafkaMessageHeaderAccessor,
+               meta: ConsumerRecordMetadata) =
+        log.info("Mottatt innsending ${innsending.id} på ${meta.topic()} for ${accessor.nonBlockingRetryDeliveryAttempt.let { "$it." }} gang.")
+            .also { edi.send(innsending) }
+
+    @DltHandler
+    fun dlt(innsending: Innsending,
+            @Header(DLT_EXCEPTION_MESSAGE) msg: String,
+            @Header(DLT_EXCEPTION_STACKTRACE) trace: String) =
+        log.error("$msg ${innsending.id}  ${cfg.topics.retries} forsøk ($trace)")
+}
--- src/main/kotlin/no/nav/helseidnavtest/edi20/Recoverability.kt
+++ src/main/kotlin/no/nav/helseidnavtest/edi20/Recoverability.kt
@@ -1,24 +1,12 @@
 package no.nav.helseidnavtest.edi20
 
 import no.nav.helseidnavtest.edi20.InnsendingConfig.Companion.INNSENDING
-import no.nav.helseidnavtest.error.IrrecoverableException
-import no.nav.helseidnavtest.oppslag.adresse.Innsending
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.NestedConfigurationProperty
-import org.springframework.kafka.annotation.DltHandler
-import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.annotation.RetryableTopic
-import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata
 import org.springframework.kafka.retrytopic.DestinationTopic.Properties
-import org.springframework.kafka.retrytopic.RetryTopicHeaders.DEFAULT_HEADER_ATTEMPTS
 import org.springframework.kafka.retrytopic.RetryTopicNamesProviderFactory
 import org.springframework.kafka.retrytopic.RetryTopicNamesProviderFactory.RetryTopicNamesProvider
 import org.springframework.kafka.retrytopic.SuffixingRetryTopicNamesProviderFactory.SuffixingRetryTopicNamesProvider
-import org.springframework.kafka.support.KafkaHeaders.DLT_EXCEPTION_MESSAGE
-import org.springframework.kafka.support.KafkaHeaders.DLT_EXCEPTION_STACKTRACE
-import org.springframework.messaging.handler.annotation.Header
-import org.springframework.retry.annotation.Backoff
 import org.springframework.stereotype.Component
 
 @Component
@@ -56,7 +44,7 @@ abstract class KafkaConfig(val name: String, val isEnabled: Boolean) {
     abstract fun topics(): List<String>
 }
 
-@Component
+//@Component
 class InnsendingRetryTopicNamingProviderFactory(private val cf: InnsendingConfig) : RetryTopicNamesProviderFactory {
 
     override fun createRetryTopicNamesProvider(p: Properties): RetryTopicNamesProvider {
@@ -78,25 +66,3 @@ class InnsendingRetryTopicNamingProviderFactory(private val cf: InnsendingConfig
     }
 }
 
-@Component
-class BestillingHendelseKonsument(private val edi: EDI20Service, val cfg: InnsendingConfig) {
-
-    private val log = getLogger(BestillingHendelseKonsument::class.java)
-
-    @KafkaListener(topics = ["#{@innsendingConfig.topics.main}"], containerFactory = INNSENDING)
-    @RetryableTopic(attempts = "#{@innsendingConfig.topics.retries}",
-        backoff = Backoff(delayExpression = "#{@innsendingConfig.topics.backoff}"),
-        exclude = [IrrecoverableException::class],
-        autoStartDltHandler = "true",
-        autoCreateTopics = "false")
-    fun listen(innsending: Innsending, @Header(DEFAULT_HEADER_ATTEMPTS, required = false) antall: Int?,
-               meta: ConsumerRecordMetadata) =
-        log.info("Mottatt innsending ${innsending.id} på ${meta.topic()} for ${antall?.let { "$it." } ?: "1."} gang.")
-            .also { edi.send(innsending) }
-
-    @DltHandler
-    fun dlt(innsending: Innsending,
-            @Header(DLT_EXCEPTION_MESSAGE) msg: String,
-            @Header(DLT_EXCEPTION_STACKTRACE) trace: String) =
-        log.error("$msg ${innsending.id}  ${cfg.topics.retries} forsøk ($trace)")
-}
